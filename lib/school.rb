e14ad7d2f6f8ea842743d870050e6f39c255ca5a
Danielle Ellis <thedanielleellis@gmail.com>
2020-07-13 19:37:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,14 @@
 class School 
-  attr_accessor:roster, 
+  attr_accessor:roster, grade 
   roster{}
   def initialize(name)
+    @name=name 
   end 
   def add_student(name,grade)
+    @name=name 
+    @grade=grade
   end 
+  def sort 
+    @sort=sort 
+  end
 end 
